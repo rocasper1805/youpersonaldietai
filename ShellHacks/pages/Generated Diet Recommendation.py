364c5db842b6cc53cb5dcd01e1982c1927b73904
rocasper1805 <rocasper1805@gmail.com>
2024-10-03 11:59:32 -0400

    Update Generated Diet Recommendation.py

--- ShellHacks/pages/Generated Diet Recommendation.py
+++ ShellHacks/pages/Generated Diet Recommendation.py
@@ -35,7 +35,7 @@ def set_background_image(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Set the background image
-set_background_image('/Users/robertocastillo/ShellHacks/image2.jpg')
+set_background_image('image2.jpg')
 
 # Add content
 st.markdown("<div style='text-align: center;'><h1 style='color: #353535;'>Diet Recommendation:</h1></div>", unsafe_allow_html=True)
